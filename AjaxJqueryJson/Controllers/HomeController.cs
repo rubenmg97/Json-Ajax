f4c05167f046de1e0b1d636321c99942dcb1db70
Ruben mg <ruben._.mg97@hotmail.com>
2020-07-02 21:27:44 -0500

    V INICIAL

---
+++ AjaxJqueryJson/Controllers/HomeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiDev.Bussines.Persona;
+using TiDev.Entity.Persona;
+
+namespace AjaxJqueryJson.Controllers
+{
+    public class HomeController : Controller
+    {
+        BusPersona Comando = new BusPersona();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Obtener()
+        {
+            try
+            {
+                List<EntPersona> lsPersonas = Comando.Obtener();
+                return Json(new{mensaje="ok", ls = lsPersonas }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+            //    List<EntPersona> lsPersonas = new List<EntPersona> ();
+                return Json(new { mensaje = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult ObtenerPorId(int id)
+        {
+            try
+            {
+                EntPersona Persona = Comando.Obtener(id);
+                return Json(new { mensaje = "ok", Ent = Persona }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensaje = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Agregar(EntPersona p)
+        {
+            try
+            {
+                Comando.Create(p);
+                return Json(new { mensaje = "ok"}, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensaje = ex.Message },JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public JsonResult BorrarDeServidor(int id)
+        {
+            try
+            {
+                Comando.Delete(id);
+                return Json(new { mensaje = "ok" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensaje = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Editar (EntPersona p)
+        {
+            try
+            {
+                Comando.Edit(p);
+                return Json(new { mensaje = "ok" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensaje = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Buscar(String valor)
+        {
+            try
+            {
+                List<EntPersona> lsPersonas = Comando.Obtener(valor);
+                return Json(new { mensaje = "ok", ls = lsPersonas }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { mensaje = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+    }
+}
---
+++ BussinesPersona/BusPersona.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiDev.Data.Persona;
+using TiDev.Entity.Persona;
+
+namespace TiDev.Bussines.Persona
+{
+    public class BusPersona
+    {
+        DatPersona data = new DatPersona();
+        public List<EntPersona> Obtener()
+        {
+            List<EntPersona> ls = new List<EntPersona>();
+            DataTable tabla = new DataTable();
+            tabla = data.Obtener();
+            foreach (DataRow fila in tabla.Rows)
+            {
+                EntPersona persona = new EntPersona();
+                persona.Id = Convert.ToInt32(fila["Id"]);
+                persona.Nombre = fila["Nombre"].ToString();
+                persona.Paterno = fila["Paterno"].ToString();
+                persona.Materno = fila["Materno"].ToString();
+                persona.Sexo = Convert.ToBoolean(fila["Sexo"]);
+                persona.Nacimiento = Convert.ToDateTime(fila["FechaNacimiento"]);
+                persona.Alta = Convert.ToDateTime(fila["FechaAlta"]);
+                persona.Edad = Convert.ToInt32(fila["Edad"]);
+
+                ls.Add(persona);
+            }
+            return ls;
+        }
+
+        public EntPersona Obtener(int id)
+        {
+            DataRow fila = data.Obtener(id);
+            EntPersona persona = new EntPersona();
+            persona.Id = Convert.ToInt32(fila["Id"]);
+            persona.Nombre = fila["Nombre"].ToString();
+            persona.Paterno = fila["Paterno"].ToString();
+            persona.Materno = fila["Materno"].ToString();
+            persona.Sexo = Convert.ToBoolean(fila["Sexo"]);
+            persona.Nacimiento = Convert.ToDateTime(fila["FechaNacimiento"]);
+            persona.Alta = Convert.ToDateTime(fila["FechaAlta"]);
+            persona.Edad = Convert.ToInt32(fila["Edad"]);
+            return persona;
+        }
+
+        public List<EntPersona> Obtener(String nombre)
+        {
+            List<EntPersona> ls = new List<EntPersona>();
+            DataTable tabla = new DataTable();
+            tabla = data.Obtener(nombre);
+            foreach (DataRow fila in tabla.Rows)
+            {
+                EntPersona persona = new EntPersona();
+                persona.Id = Convert.ToInt32(fila["Id"]);
+                persona.Nombre = fila["Nombre"].ToString();
+                persona.Paterno = fila["Paterno"].ToString();
+                persona.Materno = fila["Materno"].ToString();
+                persona.Sexo = Convert.ToBoolean(fila["Sexo"]);
+                persona.Nacimiento = Convert.ToDateTime(fila["FechaNacimiento"]);
+                persona.Alta = Convert.ToDateTime(fila["FechaAlta"]);
+                persona.Edad = Convert.ToInt32(fila["Edad"]);
+
+                ls.Add(persona);
+            }
+            return ls;
+        }
+
+        public void Delete(int id)
+        {
+            int filasAfectadas = data.Delete(id);
+            if (filasAfectadas != 1)
+            {
+                throw new ApplicationException("Error al Eliminar Persona");
+            }
+        }
+
+        public void Edit(EntPersona p)
+        {
+            int filasAfectadas = data.Edit(p.Id, p.Nombre, p.Paterno, p.Materno, p.Nacimiento, p.Sexo);
+            if (filasAfectadas != 1)
+            {
+                throw new ApplicationException("Error al Editar Persona");
+            }
+        }
+
+        public void Create(EntPersona p)
+        {
+            //if (data.Obtener(p.Nombre, p.Paterno, p.Materno))
+            //{
+            int filasAfectadas = data.Create(p.Nombre, p.Paterno, p.Materno, p.Nacimiento, p.Sexo);
+            if (filasAfectadas != 1)
+            {
+                throw new ApplicationException("Error al Crear Persona");
+            }
+            //}
+            //else
+            //{
+            //throw new ApplicationException("Error Persona Existente");
+            //}
+        }
+    }
+}
